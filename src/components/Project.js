5eb101f764a131ca2a11fae15ae2f0f003658890
Urmit Patel <up112@protonmail.com>
2020-05-06 17:10:30 -0400

    fixed projects view on mobile

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,28 +3,24 @@ import React from "react";
 function Project({ projects }) {
   const projectsDom = projects.map((project, index) => {
     return (
-      <div className={"card text-dark bg-light mb-3"}>
-        <h5
-          className={"card-header text-center stretched-link"}
-          href={project.githubUrl}
-        >
+      <div className={"card text-dark bg-light"}>
+        <h5 className={"card-header text-center "} href={project.githubUrl}>
           {project.name}
         </h5>
         <div className={"card-body border-info"}>
           <p className={"card-text"}>
             <small class={"font-weight-light"}>{project.summary}</small>
           </p>
+          <div className={" text-center"}>
+            {project.languages.map((language) => (
+              <span className={"badge badge-secondary"}>{language}</span>
+            ))}
+          </div>
         </div>
-        <div className={" text-center"}>
-          {project.languages.map((language) => (
-            <span className={"badge badge-secondary"}>{language}</span>
-          ))}
-        </div>
-        <br />
       </div>
     );
   });
-  return <div className={"project-wrapper"}>{projectsDom}</div>;
+  return <div className={"card-columns"}>{projectsDom}</div>;
 }
 
 export default Project;
