95b316a1df0f8d805e2b2766aaeccbc4dff9f001
Urmit Patel <up112@protonmail.com>
2020-05-12 18:58:50 -0400

    profile picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
+import me from "../newprofile.png";
 
 function About({ basics, education }) {
   return (
     <div className={"text-light"}>
+      <div class="text-center">
+        <img src={me} className="rounded-circle border border-light" alt="" />
+      </div>
       <h2 className={"page-section-heading text-center"}>
         Hey! I'm {basics.name} and currently work as a {basics.label}.
       </h2>
